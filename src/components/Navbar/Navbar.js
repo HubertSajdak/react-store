e6004c616d61918b772bb93d9d7e7580c4d6c7e5
Hubert <hub.sajdak@gmail.com>
2022-03-12 20:35:53 +0100

    added smallscreen store options

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { AiOutlineBars, AiOutlineClose } from 'react-icons/ai'
@@ -8,21 +8,73 @@ import './Navbar.css'
 const Navbar = () => {
 	const counter = useSelector(state => state.cart.totalQuantity)
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const menuRef = useRef()
+	useEffect(() => {
+		let handler = e => {
+			if (!menuRef.current.contains(e.target)) {
+				setIsMenuOpen(false)
+			}
+		}
+		document.addEventListener('mousedown', handler)
 
+		return () => {
+			document.removeEventListener('mousedown', handler)
+		}
+	}, [])
 	return (
 		<nav className='navbar'>
 			<div className='navbar__wrapper'>
 				<Link to='/welcome' className='navbar__brand-name'>
 					<span>Clothing</span> Store
 				</Link>
-				<div className='navbar__menu'>
-					<AiOutlineBars className='navbar__menu--open-icon' />
+				<div className='navbar__smallscreen-menu'>
+					<AiOutlineBars className='navbar__menu--open-icon' onClick={() => setIsMenuOpen(!isMenuOpen)} />
+					<ul
+						className='navbar__smallscreen-menu-links'
+						ref={menuRef}
+						style={isMenuOpen ? { transform: 'translateX(0)' } : { transform: 'translateX(-110%)' }}>
+						<AiOutlineClose className='navbar__menu--close-icon' onClick={() => setIsMenuOpen(!isMenuOpen)} />
+						<li>
+							<Link to='/welcome' onClick={() => setIsMenuOpen(false)}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to='/store' onClick={() => setIsMenuOpen(false)}>
+								Store
+							</Link>
+						</li>
+						<li>
+							<Link to='/about' onClick={() => setIsMenuOpen(false)}>
+								About Us
+							</Link>
+						</li>
+					</ul>
 				</div>
+
+				<ul className='navbar__links'>
+					<li>
+						<Link to='/welcome' onClick={() => setIsMenuOpen(false)}>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to='/store' onClick={() => setIsMenuOpen(false)}>
+							Store
+						</Link>
+					</li>
+					<li>
+						<Link to='/about' onClick={() => setIsMenuOpen(false)}>
+							About Us
+						</Link>
+					</li>
+				</ul>
+
 				<div className='navbar__customer-panel'>
-					<div className='navbar__customer-panel_cart'>
+					<NavLink to='/cart' className='navbar__customer-panel_cart'>
 						<div className='navbar__customer-panel--cart-counter'>{counter}</div>
 						<BsCart2 className='navbar__customer-panel--cart-icon' />
-					</div>
+					</NavLink>
 					<div className='navbar__customer-panel_profile'>
 						<CgProfile className='navbar__customer-panel--profile-icon' />
 					</div>
--- src/components/Shop/ShopNavigation/ShopNavigation.js
+++ src/components/Shop/ShopNavigation/ShopNavigation.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux'
 import { sortingActions } from '../../../store/sorting-slice'
 import { filteringActions } from '../../../store/filtering-slice'
 import { FaThList, FaBoxes } from 'react-icons/fa'
-import { BsFillArrowDownSquareFill } from 'react-icons/bs'
+import { BsFillArrowDownSquareFill, BsSortUp } from 'react-icons/bs'
 import './ShopNavigation.css'
 const ShopNavigation = props => {
 	const [isMenuOpen, setIsMenuOpen] = useState([])
@@ -45,58 +45,56 @@ const ShopNavigation = props => {
 	}
 
 	return (
-		<div className='shop-navigation'>
-			<div className={isMenuOpen ? 'shop-navigation__wrapper' : 'shop-navigation__wrapper menu__hidden'}>
-				<div className='shop-navigation__sort'>
-					<label htmlFor='sort'>Sort</label>
-					<select onChange={sortHandler} name='sort' id='sorting' defaultValue='none' ref={typeRef}>
-						<option value='none'>-</option>
-						<option value='priceAsc'>Lowest Price</option>
-						<option value='priceDes'>Highest Price</option>
-						<option value='titleAsc'>A-Z</option>
-						<option value='titleDes'>Z-A</option>
-					</select>
-				</div>
-				<hr />
-				<div className='shop-navigation__category'>
-					<p>Categories</p>
-					<ul className='shop-navigation__category--box'>
-						<li>
-							<input type='checkbox' id='backpacks' value='backpack' onClick={filterHandler} />
-							<label htmlFor='backpacks'>Backpacks</label>
-						</li>
-						<li>
-							<input type='checkbox' id='jackets' value='jacket' onClick={filterHandler} />
-							<label htmlFor='jackets'>Jackets</label>
-						</li>
-						<li>
-							<input type='checkbox' id='t-shirts' value='shirt' onClick={filterHandler} />
-							<label htmlFor='t-shirts'>T-Shirts</label>
-						</li>
-						<li>
-							<input type='checkbox' id='sleeves' value='sleeve' onClick={filterHandler} />
-							<label htmlFor='sleeves'>Sleeves</label>
-						</li>
-						<li>
-							<input type='checkbox' id='casual' value='casual' onClick={filterHandler} />
-							<label htmlFor='casual'>Casual</label>
-						</li>
-					</ul>
-				</div>
-				<hr />
-				<div className='shop-navigation__view'>
-					<FaThList className='shop-navigation__view--list' />
-					<FaBoxes className='shop-navigation__view--box' />
+		<>
+			<div className={!isMenuOpen ? 'shop-navigation' : 'shop-navigation menu__hidden'}>
+				<div className='shop-navigation__wrapper'>
+					<div className='shop-navigation__sort'>
+						<label htmlFor='sort'>Sort</label>
+						<select onChange={sortHandler} name='sort' id='sorting' defaultValue='none' ref={typeRef}>
+							<option value='none'>-</option>
+							<option value='priceAsc'>Lowest Price</option>
+							<option value='priceDes'>Highest Price</option>
+							<option value='titleAsc'>A-Z</option>
+							<option value='titleDes'>Z-A</option>
+						</select>
+					</div>
+					<hr />
+					<div className='shop-navigation__category'>
+						<p>Categories</p>
+						<ul className='shop-navigation__category--box'>
+							<li>
+								<input type='checkbox' id='backpacks' value='backpack' onClick={filterHandler} />
+								<label htmlFor='backpacks'>Backpacks</label>
+							</li>
+							<li>
+								<input type='checkbox' id='jackets' value='jacket' onClick={filterHandler} />
+								<label htmlFor='jackets'>Jackets</label>
+							</li>
+							<li>
+								<input type='checkbox' id='t-shirts' value='shirt' onClick={filterHandler} />
+								<label htmlFor='t-shirts'>T-Shirts</label>
+							</li>
+							<li>
+								<input type='checkbox' id='sleeves' value='sleeve' onClick={filterHandler} />
+								<label htmlFor='sleeves'>Sleeves</label>
+							</li>
+							<li>
+								<input type='checkbox' id='casual' value='casual' onClick={filterHandler} />
+								<label htmlFor='casual'>Casual</label>
+							</li>
+						</ul>
+					</div>
+					<hr />
+					<div className='shop-navigation__view'>
+						<FaThList className='shop-navigation__view--list' />
+						<FaBoxes className='shop-navigation__view--box' />
+					</div>
 				</div>
 			</div>
 			<div className='shop-navigation__smallscreen-menu '>
-				<BsFillArrowDownSquareFill
-					className='shop-navigation__smallscreen-menu--show-icon'
-					style={isMenuOpen && { transform: 'rotate(180deg)' }}
-					onClick={menuHandler}
-				/>
+				<BsSortUp className='shop-navigation__smallscreen-menu--show-icon' onClick={menuHandler} />
 			</div>
-		</div>
+		</>
 	)
 }
 export default ShopNavigation
